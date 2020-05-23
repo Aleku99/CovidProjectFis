fca1cc44843e1f1348efbac1ea8ca07045de0c1f
Roxana <roxanalala7@gmail.com>
2020-05-23 15:44:25 +0300

    Added write symptoms functionality

--- src/main/java/Pacient.java
+++ src/main/java/Pacient.java
@@ -27,9 +27,11 @@ import sun.security.krb5.internal.crypto.Aes128;
 import javax.xml.soap.Text;
 
 public class Pacient {
+
+    public static String pacientName="";
     public static Scene pacientStartScene(String name)
     {
-
+        pacientName = name;
         Label titleLabel = new Label("Welcome " + name);
         Button logoutButton = new Button();
         Button writeSymptomsButton = new Button();
@@ -40,7 +42,7 @@ public class Pacient {
         checkTreatmentButton.setText("Check treatment");
 
         logoutButton.setOnAction(e->{Main.window.setScene(User.login());});
-        writeSymptomsButton.setOnAction(e->{});
+        writeSymptomsButton.setOnAction(e->{Main.window.setScene(writeSymptomsScene());});
         checkTreatmentButton.setOnAction(e->{});
 
         GridPane grid = new GridPane();
@@ -56,5 +58,78 @@ public class Pacient {
         Scene pacientStartScene = new Scene(grid, 400, 600);
         return pacientStartScene;
     }
+
+    public static Scene writeSymptomsScene()
+    {
+        Label writeSymptomsLabel = new Label("Write your symtpoms in the box below");
+        TextField writeSymtpomsTextField = new TextField();
+        Button backButton = new Button();
+        Button submitButton = new Button();
+
+        submitButton.setText("Submit");
+        backButton.setText("Back");
+        submitButton.setOnAction(e->{
+            updateDb("symptoms", writeSymtpomsTextField.getText());
+            Main.window.setScene(writeSymptomsScene());
+        });
+        backButton.setOnAction(e->Main.window.setScene(pacientStartScene(pacientName)));
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(20,20,20,20));
+        grid.setVgap(10);
+        GridPane.setConstraints(writeSymptomsLabel, 1,0);
+        GridPane.setConstraints(writeSymtpomsTextField, 1,1);
+        GridPane.setConstraints(submitButton, 1,2);
+        GridPane.setConstraints(backButton, 1,3);
+
+        grid.getChildren().addAll(writeSymptomsLabel, writeSymtpomsTextField, submitButton, backButton);
+        Scene writeSymptomsScene = new Scene(grid, 400, 600);
+        return writeSymptomsScene;
+    }
+
+    public static void updateDb(String key, String value)
+    {
+         JSONObject pacientUsersObj = new JSONObject();
+         JSONArray pacientUsersArray = new JSONArray();
+
+        JSONParser jsonParser = new JSONParser();
+        try
+        {
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("pacientdb.json"));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("users");
+
+
+            for(Object o:jsonArray)
+            {
+
+                if(((JSONObject)o).get("name").equals(pacientName))
+                {
+                    ((JSONObject)o).put("symptoms", value);
+                    System.out.println(value);
+                    System.out.println(((JSONObject)o).get("user"));
+                    pacientUsersArray.add(o);
+
+                }
+                else
+                {
+                    pacientUsersArray.add(o);
+                }
+            }
+        }
+        catch(Exception e2)
+        {
+            e2.printStackTrace();
+        }
+        pacientUsersObj.put("users", pacientUsersArray);
+
+
+        try (FileWriter file = new FileWriter("pacientdb.json")) {
+            file.write(pacientUsersObj.toJSONString());
+            file.flush();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+
 
 }
